558da625cc9e2a5d32e868c3e29cd785034df8fe
david <swordsareus@gmail.com>
2019-06-04 22:36:01 +0930

    Added a .gitignore for the cloned repos dir and updated the script

--- script.sh
+++ script.sh
@@ -1,46 +1,54 @@
 #!/bin/bash
 
-
-
 create_output () {
 	dataset_file=""
-	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"	#Gets path of script
+	dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"	#Gets path of script
 	if [[ "$1" != "SConstruct" && "$1" != "Makefile" ]];then
 		echo "$1 is not a valid parameter. Please pass in either 'Makefile' or 'SConstruct'"
 		return 1
 	else
-		dataset_file="$DIR/$1-project-list.txt"
+		dataset_file="$dir/$1-project-list.txt"
 	fi
 	mkdir -p "results"
-	mkdir -p "repos"
+	mkdir -p "repos/$1"
 
 	output_filename="results/$1.csv"
 
-	# return 0
-
-	cd "repos"
+	#Download the projects
+	cd "repos/$1"
+	counter=0
 	cat $dataset_file | while read x;do
-		# git clone $x
-		echo $x
+		echo -e "$1 project number \"$counter\":"
+		let counter=counter+1
+		git clone $x
+		# echo $x
 	done
 	error_code=$?	#If an error occurs in the while loop, this will detect it
-	cd "../"
 	if [[ $error_code != 0 ]];then
 		exit "$error_code"
 	fi
 
+	cd $dir
 	return 0
 
+	#Explore each project
 	ls "$PWD" | while read x;do
-		#Explore each of the projects
-		echo "Test"
+		project_name=$x
+		cd "$x"
+		find . -name "$1" | while read y;do
+			echo "$y"
+		done
+		cd "../"
 	done
 	error_code=$?	#If an error occurs in the while loop, this will detect it
 	if [[ $error_code != 0 ]];then
-		exit "$errorCode"
+		exit "$error_code"
 	fi
+	cd $dir
 }
 
 create_output "SConstruct"
+echo "Done SConstruct"
 create_output "Makefile"
-create_output "DERP"
+echo "Done Makefile"
+# create_output "DERP"
